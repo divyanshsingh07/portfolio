6b16c782aae85332fad0cf569cf0e77a2d011eaa
Divyansh Singh <divyanshsingh@Divyanshs-MacBook-Air.local>
2024-07-18 19:42:29 +0530

    adding project section

--- script.js
+++ script.js
@@ -27,8 +27,25 @@ navLinks.forEach(n => n.addEventListener("click", linkAction));
 
 
 /*=============== SWIPER PROJECTS ===============*/
+let swiperProjects = new Swiper(".project-container", {
 
+    loop: true,
+    spaceBetween: 24,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: -56,
+        },
 
+    },
+});
 /*=============== SWIPER TESTIMONIAL ===============*/
 
 
